eb3f6b11ff09221c18617c3dde043c5f3d154dc1
github username <github email address>
2019-10-07 17:20:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit",function(event){
+      event.preventDefault();
+      const tasks = document.getElementById("tasks");
+      const taskToAdd = document.createElement("li");
+      const priority = document.getElementById("priority").value;
+      const button = document.createElement("button");
+      button.addEventListener("click",function(event) {
+        button.parentNode.remove();
+      });
+      button.innerHTML = "X";
+      button.setAttribute("id", "delete-list-item");
+      set_priority(taskToAdd,priority);
+      taskToAdd.innerText = document.getElementById("new-task-description").value;
+      taskToAdd.appendChild(button);
+      tasks.appendChild(taskToAdd);
+  })    
 });
+
+function set_priority(task,priority) {
+  switch (priority) {
+    case "high":
+    task.style.color = "red";
+    break;
+    case "low":
+      task.style.color = "green";
+    break;
+    default: 
+      task.style.color = "gold";
+  } 
+}
