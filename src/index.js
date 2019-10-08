1b2e766bb88a5c2a4e70c25989c785294e9f23e0
github username <github email address>
2019-10-08 09:18:27 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const form = document.getElementById("create-task-form");
-  form.addEventListener('submit', function(e){
+  // const form = document.getElementById("create-task-form");
+  document.addEventListener('click', function(e){
     e.preventDefault();
-    const newTaskName = document.getElementById('new-task-description');
-    const newTask = document.createElement('li');
-    newTask.innerText = newTaskName.value;
-    document.getElementById('tasks').appendChild(newTask);
-    e.target.reset();
+    // console.log(event.target)
+    if (event.target.value === "Create New Task") {
+      console.log(event.target.value)
+      const newTaskName = document.getElementById('new-task-description');
+      const newTask = document.createElement('li');
+      const newbutton = document.createElement('button');
+      newbutton.setAttribute('id', newTaskName.value);
+      // newbutton.data-description = newTaskName.value;
+      newTask.innerText = newTaskName.value;
+      newbutton.innerText = "X";
+      newbutton.className = "delete";
+      document.getElementById('tasks').appendChild(newTask).appendChild(newbutton);
+      newTaskName.value = "";
+    } else if (event.target.className === "delete") {
+      document.getElementById(event.target.id).parentElement.remove();
+    } 
   });
+  // form.addEventListener('button', function(e){
+
+  // });
 
 });
 
