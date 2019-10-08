ba2957dfc230b9e64855021c7edd19a6889d1c8f
castagnaTM <castagna.tm@gmail.com>
2019-10-08 09:17:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-
  //find the task form
  const taskSubmitForm = document.getElementById('create-task-form');
-
  //add event listener for submit
 taskSubmitForm.addEventListener('submit', newTasks);
 });
 
-
 const newTasks = event =>{
   //prevents normal submission
   event.preventDefault();
-
   //creates new list elements
-  const task = document.createElement('Li');
+  const listedTask = document.createElement('Li');
   //sets list element text to the value attached to the label by element-id
-  task.innerText = document.getElementById("new-task-description").value;
+  listedTask.innerText = document.getElementById("new-task-description").value;
 //attaches that task to the task ordered list
-  document.getElementById('tasks').appendChild(task);
+  document.getElementById('tasks').appendChild(listedTask);
 }
