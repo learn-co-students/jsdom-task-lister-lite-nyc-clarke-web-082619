2a9ce76014f146f8f69db58e51cf3012ed77c1b0
jw20191n <clairejw1015@gmail.com>
2019-10-08 13:55:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  let ul = document.getElementById('tasks');
+
+  form.addEventListener("submit", function(event){
+    let newLi = document.createElement('li');
+    let value = document.getElementById('new-task-description').value;
+    newLi.innerText = value;
+    ul.appendChild(newLi);
+    event.preventDefault();
+  });
 });
